42a4d4f3b7900f0a4aaf016838bcaaab1e861f39
Lyla M. Fujiwara <lmf@google.com>
2017-08-16 21:17:34 -0700

    Step: Create the Database
    Change-Id: I2d55a10e51af289706386fa41684575f0c7c70ee

---
+++ app/src/main/java/com/example/android/sunshine/data/database/SunshineDatabase.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.sunshine.data.database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
+import android.content.Context;
+import android.util.Log;
+
+/**
+ * {@link SunshineDatabase} database for the application including a table for {@link WeatherEntry}
+ * with the DAO {@link WeatherDao}.
+ */
+
+// List of the entry classes and associated TypeConverters
+@Database(entities = {WeatherEntry.class}, version = 1)
+@TypeConverters(DateConverter.class)
+public abstract class SunshineDatabase extends RoomDatabase {
+
+    private static final String LOG_TAG = SunshineDatabase.class.getSimpleName();
+    private static final String DATABASE_NAME = "weather";
+
+    // For Singleton instantiation
+    private static final Object LOCK = new Object();
+    private static SunshineDatabase sInstance;
+
+    public static SunshineDatabase getInstance(Context context) {
+        Log.d(LOG_TAG, "Getting the database");
+        if (sInstance == null) {
+            synchronized (LOCK) {
+                sInstance = Room.databaseBuilder(context.getApplicationContext(),
+                        SunshineDatabase.class, SunshineDatabase.DATABASE_NAME).build();
+                Log.d(LOG_TAG, "Made new database");
+            }
+        }
+        return sInstance;
+    }
+
+    // The associated DAOs for the database
+    public abstract WeatherDao weatherDao();
+}
